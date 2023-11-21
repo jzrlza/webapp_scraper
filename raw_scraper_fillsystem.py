7b40889cd700fa6dc8369964bb33985e71ffffb6
jzrlza <parinvut12345@gmail.com>
2023-11-21 15:08:03 +0700

    #

--- raw_scraper_fillsystem.py
+++ raw_scraper_fillsystem.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
+#from selenium.common.exceptions import NoSuchElementException
 import re
 import time
 import json
