ce76d6a50850f52c7762236a150519730e4d0705
jzrlza <parinvut12345@gmail.com>
2023-11-28 11:20:49 +0700

    next chapter

--- raw_scraper_boardband.py
+++ raw_scraper_boardband.py
@@ -30,47 +30,22 @@ mock_request = """{
    "predefined_g_no_if_free": "4G",
    "urls":[
       {
-         "url_link":"https://www.ais.th/consumers/fibre/package",
-         "operator_id":0,
+         "url_link":"https://www.true.th/trueonline/package-types/true-gigatex-pro-special-ssv/",
+         "operator_id":2,
          "pricing_type":2,
-         "track_new_mega_row": false,
+         "track_new_mega_row": true,
          "collect_sub_urls": true,
          "urls_class_type_id": 0,
-         "plans":[],
-         "plans_template": {
-             "plan_name":"",
-             "capture_sub_names": false,
-             "capture_mode":0,
-             "has_extra_table":false,
-             "has_term_and_condition":false
-         },
+         "plans":[
+            {
+               "plan_name":"temp",
+               "capture_sub_names": false,
+               "capture_mode":0,
+               "has_extra_table":false,
+               "has_term_and_condition":false
+            }
+         ],
          "special_case_plans": []
-      },
-      {
-         "url_link":"https://www.ais.th/consumers/fibre",
-         "operator_id":0,
-         "pricing_type":2,
-         "track_new_mega_row": false,
-         "collect_sub_urls": true,
-         "urls_class_type_id": 1,
-         "plans":[],
-         "plans_template": {
-             "plan_name":"",
-             "capture_sub_names": false,
-             "capture_mode":0,
-             "has_extra_table":true,
-             "has_term_and_condition":false
-         },
-         "special_case_plans": [
-	         {
-	             "plan_name":"Smart AI Gamer",
-	             "sub_url": "https://www.ais.th/consumers/fibre/package/smart-ai-gamer/",
-	             "capture_sub_names": false,
-	             "capture_mode":1,
-	             "has_extra_table":true,
-	             "has_term_and_condition":false
-	         }
-         ]
       }
    ],
    "webdriver_timeout":15
