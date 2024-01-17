5f2719f403ff3746a948a68e4059bd9d50f410cf
jzrlza <parinvut12345@gmail.com>
2024-01-17 10:12:34 +0700

    new file

---
+++ direct_csv_exporter.py
@@ -0,0 +1,6 @@
+import boardband_ais
+import web_scraper_boardband
+
+input_request = boardband_ais.request
+
+raw_result_string = web_scraper_boardband.scrape_web(input_request, normalize_result=True)
